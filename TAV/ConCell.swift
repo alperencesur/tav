9e6bdafc34e11860eab0b0f723d07ccf61c8706d
alpicesur <32032170+alpicesur@users.noreply.github.com>
2018-04-15 12:08:29 +0300

    first commit

---
+++ TAV/ConCell.swift
@@ -0,0 +1,23 @@
+//
+//  ConCell.swift
+//  
+//
+//  Created by Alperen Cesur on 15.04.2018.
+//
+
+import UIKit
+
+class ConCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
